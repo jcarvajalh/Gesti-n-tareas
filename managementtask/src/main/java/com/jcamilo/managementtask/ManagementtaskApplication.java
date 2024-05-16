79d408733730894e5b0aa447a1e4083055ba17a0
juan_camilo <caamiloc@outlook.com>
2024-05-16 12:46:41 -0500

    initial app

---
+++ managementtask/src/main/java/com/jcamilo/managementtask/ManagementtaskApplication.java
@@ -0,0 +1,13 @@
+package com.jcamilo.managementtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementtaskApplication.class, args);
+	}
+
+}
---
+++ managementtask/src/test/java/com/jcamilo/managementtask/ManagementtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcamilo.managementtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
